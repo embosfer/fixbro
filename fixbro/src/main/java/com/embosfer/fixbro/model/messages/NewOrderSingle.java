56b6cac37a239742c08a04e368c39cfecb442681
embosfer <e.bosch.ferrando@gmail.com>
2016-07-09 16:44:37 +0100

    Adding logger and MessageCracker, with NOS callback

---
+++ fixbro/src/main/java/com/embosfer/fixbro/model/messages/NewOrderSingle.java
@@ -0,0 +1,38 @@
+/***********************************************************************************************************************
+ *
+ * FixBro - an open source FIX broker (sell side) simulator
+ * =========================================================
+ *
+ * Copyright (C) 2016 by Emilio Bosch Ferrando
+ * https://github.com/embosfer
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
+package com.embosfer.fixbro.model.messages;
+
+import com.embosfer.fixbro.model.tags.OrdType;
+
+/**
+ * Class representing a 35=D message
+ * 
+ * @author embosfer
+ *
+ */
+public class NewOrderSingle {
+
+//	private final String clOrdID; // 11
+//	private final String symbol; // 55
+//	private final double price; // 44
+//	private final double orderQty; // 38
+//	private final OrdType ordType; // 40
+}
