c18e68da6d7c2aad6a536b4417c275466ef883ea
embosfer <e.bosch.ferrando@gmail.com>
2016-07-23 19:31:25 +0100

    Add ER observer and observable interfaces

---
+++ fixbro/src/main/java/com/embosfer/fixbro/controller/er/observer/ExecutionReportObservable.java
@@ -0,0 +1,32 @@
+/***********************************************************************************************************************
+ *
+ * FixBro - an open source FIX broker (sell side) simulator
+ * =========================================================
+ *
+ * Copyright (C) 2016 by Emilio Bosch Ferrando
+ * https://github.com/embosfer
+ *
+ ***********************************************************************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ ***********************************************************************************************************************/
+package com.embosfer.fixbro.controller.er.observer;
+
+/**
+ * @author embosfer
+ *
+ */
+public interface ExecutionReportObservable {
+
+	void registerExecutionReportObservable(ExecutionReportObserver observer);
+
+	void unRegisterExecutionReportObservable(ExecutionReportObserver observer);
+}
